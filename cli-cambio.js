53bf89d0d87eefb01c76d32d96d27833b2e62d11
Bianca Amarante Magalhães <164433450+byancaaam@users.noreply.github.com>
2025-08-07 23:16:09 -0300

    fix: verifica se o valor foi digitado, se é um número, se  é positivo e diferente de zero

--- cli-cambio.js
+++ cli-cambio.js
@@ -8,8 +8,10 @@ function isValidCurrency(code) {
   return /^[a-zA-Z]{3}$/.test(code);
 }
 
-if (!valor || isNaN(valor)) {
-  console.log('⚠️  É necessário informar um valor numérico para conversão!');
+if (!valor || isNaN(valor) || valor <= 0) {
+  console.log(
+    '⚠️  Insira um valor positivo e diferente de zero para o valor a ser convertido!'
+  );
   process.exit(1);
 }
 
@@ -23,6 +25,13 @@ if (!moedaOrigem || !isValidCurrency(moedaOrigem)) {
 if (!moedaDestino || !isValidCurrency(moedaDestino)) {
   console.log(
     '⚠️  É necessário informar um valor válido moeda de destino para a conversão! - Ex: USD, BRL'
+  );
+  process.exit(1);
+}
+
+if (moedaOrigem === moedaDestino) {
+  console.log(
+    '⚠️  O tipo da moeda de origem é igual ao da moeda de destino, tente novamente!'
   );
   process.exit(1);
 }
